cf188b95d8e20d2c6f0d039297f37e3bec07f27e
ABHIGYAN-MOHANTA <underthunder02@gmail.com>
2022-09-16 21:35:09 +0530

    Initial commit

---
+++ date.js
@@ -0,0 +1,24 @@
+//can use just exports. instead of module.exports.
+
+module.exports.getDate = function() {
+    const today = new Date();
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+    };
+    return today.toLocaleDateString("en-US", options);
+  
+  };
+
+
+
+module.exports.getDay = function() {
+    const today = new Date();
+    const options = {
+      weekday: "long",
+    };
+    return today.toLocaleDateString("en-US", options);
+    
+  };
+
